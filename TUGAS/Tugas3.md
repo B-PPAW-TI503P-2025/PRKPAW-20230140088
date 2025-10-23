9efd97deaf2c91a3f23a95510daf32b8e75d78fc
dzakiyahalzahrani <dzakiyahalzahrani@gmail.com>
2025-10-23 13:12:16 +0700

    Commit ketiga

---
+++ TUGAS/Tugas3.md
@@ -0,0 +1,16 @@
+# Tugas 3
+
+presensi/check-in :
+![alt text](<Presensi check in.png>)
+
+presensi/check-in jika dilakukan lebih dari 1 kali :
+![alt text](<Presensi check in lebih dari 1x.png>)
+
+presensi/check-out :
+![alt text](<Presensi check in.png>)
+
+presensi/check-out jika belum melakukan check-in :
+![alt text](<Presensi checkout belum check in.png>)
+
+reports/daily :
+![alt text](<Report daily.png>)
--- pawpraktikum1_node_20230140088/server.js
+++ pawpraktikum1_node_20230140088/server.js
@@ -3,18 +3,32 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 const PORT = 3001;
+const morgan = require("morgan");
+
+// Impor router
+const presensiRoutes = require("./routes/presensi");
+const reportRoutes = require("./routes/reports");
 
 // // Middleware (HARUS DI ATAS ROUTE SPESIFIK)
 app.use(cors());
-app.use(express.json()); // <--- Body Parser untuk JSON
+app.use(express.json());
+app.use(morgan("dev")); // <--- Body Parser untuk JSON
 app.use((req, res, next) => {
   console.log(`${new Date().toISOString()} - ${req.method} ${req.url}`);
   next();
 });
 // // END Middleware
 
+const ruteBuku = require("./routes/books");
+
 // Route Spesifik (HARUS DI BAWAH Body Parser)
 app.use('/api/books', bookRoutes);
+
+// Route Spesifik untuk Presensi dan Laporan
+app.use("/api/books", ruteBuku);
+app.use("/api/presensi", presensiRoutes);
+app.use("/api/reports", reportRoutes);
+
 
 app.get('/', (req, res) => {
   res.send('Home Page for API');
